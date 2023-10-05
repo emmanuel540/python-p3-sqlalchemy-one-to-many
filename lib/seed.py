919d8fda25155eae687b3eb0105fbfd227f3add7
emmanuel540! <emmanuelhillary24@gmail.com>
2023-10-05 22:20:58 +0300

    Done and dusted

--- lib/seed.py
+++ lib/seed.py
@@ -1,55 +1,27 @@
 #!/usr/bin/env python3
 
-from faker import Faker
-import random
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from faker import Faker
+from models import Base, Game, Review
 
-from models import Game, Review
+engine = create_engine('sqlite:///one_to_many.db')
 
-if __name__ == '__main__':
-    engine = create_engine('sqlite:///one_to_many.db')
-    Session = sessionmaker(bind=engine)
-    session = Session()
+Base.metadata.bind = engine
 
-    session.query(Game).delete()
-    session.query(Review).delete()
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
-    fake = Faker()
+fake = Faker()
 
-    genres = ['action', 'adventure', 'strategy',
-        'puzzle', 'first-person shooter', 'racing']
-    platforms = ['nintendo 64', 'gamecube', 'wii', 'wii u', 'switch',
-        'playstation', 'playstation 2', 'playstation 3', 'playstation 4',
-        'playstation 5', 'xbox', 'xbox 360', 'xbox one', 'pc']
 
-    games = []
-    for i in range(50):
-        game = Game(
-            title=fake.unique.name(),
-            genre=random.choice(genres),
-            platform=random.choice(platforms),
-            price=random.randint(5, 60)
-        )
+game = Game(title=fake.catch_phrase(), genre=fake.word(), platform=fake.word(), price=fake.random_int(min=20, max=60))
+session.add(game)
+session.commit()
 
-        # add and commit individually to get IDs back
-        session.add(game)
-        session.commit()
 
-        games.append(game)
+review = Review(score=fake.random_int(min=1, max=10), comment=fake.sentence(), game=game)
+session.add(review)
+session.commit()
 
-    reviews = []
-    for game in games:
-        for i in range(random.randint(1,5)):
-            review = Review(
-                score=random.randint(0, 10),
-                comment=fake.sentence(),
-                game_id=game.id
-            )
-
-            reviews.append(review)
-    
-    session.bulk_save_objects(reviews)
-    session.commit()
-    session.close()
